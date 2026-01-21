dda13025c62cb120a2ce37edd728c6f0999d5a8b
neda-noor-m <36059982+neda-noor-m@users.noreply.github.com>
2026-01-21 19:43:34 +0000

    hi

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -3,7 +3,8 @@ import sys
 
 param = int(sys.argv[1])
 
-d = pd.DataFrame({"name":["neda", "sata"], "param": param})
+d = pd.DataFrame({"name":["neda", "sata"], "param":  [param, param]})
 print(d)
+print(f"program name:{sys.argv[0]}")
 
 d.to_parquet("file_1.parquet")
